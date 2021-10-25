67ea4089e0f4cfebfe23f84bea6aa0d4b3cd4b3e
JackPitman <80924402+JPitman26@users.noreply.github.com>
2021-10-26 00:12:31 +0100

    Update game.js
    Questions to be randomized

--- game.js
+++ game.js
@@ -13,3 +13,39 @@ let scorer = 0
 let questionCounter = 0
 let availableQuestions = []
 
+//Question list to be randomized 
+let questions = [
+    {
+        question: 'What is the smallest country in the world?',
+        choice1: 'Monaco',
+        choice2: 'San Marino',
+        choice3: 'Vatican City',
+        choice4: 'Malta',
+        answer: 3,
+    },
+    {
+        question: 'What is the longest river in the world?',
+        choice1: 'Nile',
+        choice2: 'Amazon River',
+        choice3: 'Yangtze',
+        choice4: 'None Of The Above',
+        answer: 1,
+    },
+    {
+        question: 'What colour is a giraffes tongue?',
+        choice1: 'Red',
+        choice2: 'Blue',
+        choice3: 'Purple',
+        choice4: 'Green',
+        answer: 2,
+    },
+    {
+        question: 'How many teeth does the average adult human have?',
+        choice1: '28',
+        choice2: '30',
+        choice3: '26',
+        choice4: '32',
+        answer: 4,
+    }
+]
+
