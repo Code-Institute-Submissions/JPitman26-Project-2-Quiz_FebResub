fa5df5c1ba33e9a7bf5702a384ba499b0902eed8
JackPitman <80924402+JPitman26@users.noreply.github.com>
2021-10-26 00:11:59 +0100

    Update game.js
    Sets start score to 0

--- game.js
+++ game.js
@@ -7,3 +7,5 @@ const progressBarFull = document.querySelector('#progressbarfull')
 let currentQuestion = {}
 //Allows to computer to accept answers
 let acceptingAnswers = true
+//Sets score to 0 
+let scorer = 0
