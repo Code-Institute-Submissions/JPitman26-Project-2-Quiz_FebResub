453e88df2e957c6656fb236197e2df8654763829
JackPitman <80924402+JPitman26@users.noreply.github.com>
2021-10-26 00:11:42 +0100

    Update game.js
    Allows the computer to accept answers

--- game.js
+++ game.js
@@ -3,3 +3,7 @@ const choices = Array.from(document.querySelectorAll('.choice-text'))
 const progressText = document.querySelector('#progress-text')
 const scoreText = document.querySelector('#score')
 const progressBarFull = document.querySelector('#progressbarfull')
+
+let currentQuestion = {}
+//Allows to computer to accept answers
+let acceptingAnswers = true
