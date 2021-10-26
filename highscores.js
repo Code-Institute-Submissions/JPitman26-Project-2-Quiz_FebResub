bf2ee4a722a8729df2c485b0cd92acba66a62ee0
JackPitman <80924402+JPitman26@users.noreply.github.com>
2021-10-26 11:27:27 +0100

    temp

--- highscores.js
+++ highscores.js
@@ -6,3 +6,7 @@ highScoresList.innerHTML =
     highScores.map(score => {
         return `<li class="high-score">${score.name} - ${score.score}</li>`
     }).join("")
+
+    goHomeBtn = 
+    window.location.assign('./index.html')
+    window.location.href = window.location.host.includes('github') ? '/Project-2-Quiz/' : '/index.html';
